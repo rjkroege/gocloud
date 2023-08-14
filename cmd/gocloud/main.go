6bfc0aeb7a4644a8d245ac725cba953af03f51ed
Robert Kroeger <rjkroege@liqui.org>
2023-08-14 09:03:24 -0400

    Cleanup debugging code

--- cmd/gocloud/main.go
+++ cmd/gocloud/main.go
@@ -8,9 +8,6 @@ import (
 	"github.com/alecthomas/kong"
 	"github.com/rjkroege/gocloud/config"
 	"github.com/rjkroege/gocloud/gcp"
-
-	// For debugging
-	"github.com/sanity-io/litter"
 )
 
 var CLI struct {
@@ -48,12 +45,6 @@ func main() {
 		fmt.Println("Fatai:", err)
 		os.Exit(-1)
 	}
-
-	// temp to see if the toml thing is working
-
-	litter.Dump(settings)
-
-	return
 
 	switch ctx.Command() {
 	case "ls":
